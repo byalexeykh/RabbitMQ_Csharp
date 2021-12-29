e4ff3e30ba2d68c5ea9937de4db5644565b27a1b
byalexeykh <alek8901@gmail.com>
2021-12-29 19:41:29 +0100

    init

---
+++ RMQChat/RMQClient/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace RMQClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loh = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loh
+            // 
+            this.loh.AutoSize = true;
+            this.loh.Location = new System.Drawing.Point(248, 189);
+            this.loh.Name = "loh";
+            this.loh.Size = new System.Drawing.Size(21, 13);
+            this.loh.TabIndex = 0;
+            this.loh.Text = "loh";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loh);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loh;
+    }
+}
+
