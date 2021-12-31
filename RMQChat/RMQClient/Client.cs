d62f5780c455fb0a4b09b1f50315ac299af79e1a
byalexeykh <alek8901@gmail.com>
2021-12-31 18:19:31 +0100

    Basic user and UI setup

---
+++ RMQChat/RMQClient/Client.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RabbitMQ.Client;
+
+namespace RMQClient
+{
+    // USER start
+    // THIS STRUCT WILL BE MOVED TO PACKAGE THAT I'LL CREATE LATER. aleks
+    struct UserClientInfo 
+    {
+        readonly Guid UserRoutingKey;
+        readonly string Nickname;
+
+        public UserClientInfo(Guid UserRoutingKey, string Nickname)
+        {
+            this.UserRoutingKey = UserRoutingKey;
+            this.Nickname = Nickname;
+        }
+    }
+    // USER end
+
+
+    static class Client
+    {
+        // Vars
+        private static readonly UserClientInfo UserInfo = new UserClientInfo(Guid.NewGuid(), "placeholder");
+
+        /* Client program user interface "ref"
+         * If you want to gather info form UI or change it use this var
+         */ 
+        private static ClientForm HUD;
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            HUD = new ClientForm();
+            Application.Run(HUD);
+        }
+    }
+}
--- RMQChat/RMQServer/Server.cs
+++ RMQChat/RMQServer/Server.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
 
 namespace RMQServer
 {
-    class Program
+    // USER start
+    // THIS STRUCT WILL BE MOVED TO PACKAGE THAT I'LL CREATE LATER. aleks
+    struct UserServertInfo
+    {
+        
+    }
+    // USER end
+
+    class Server
     {
         static void Main(string[] args)
         {
